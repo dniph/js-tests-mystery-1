f3c51a6f98546580c0bd4d5c7e64740e6fe4252f
Dennif <dennif.pa@gmail.com>
2025-05-21 11:05:05 -0700

    done

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,9 @@
-export const mystery1 = (num) => {};
+export const mystery1 = (num) => {
+  return num % 2 !== 0;
+};
+
+// Negative odd numbers return a truthy result
+// Odd positive numbers return a truthy result
+// Zero will return a falsy result
+// Even negative numbers must return a falsy result
+// Even positive numbers must return a falsy result
